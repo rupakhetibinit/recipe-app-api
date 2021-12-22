b734b3bd0104f5cd7fdf354d9adf0f9a19773fa7
Binit <cruiserponder@protonmail.com>
2021-12-22 21:17:07 +0545

    add order route

--- src/routes/orderRoute.js
+++ src/routes/orderRoute.js
@@ -35,4 +35,51 @@ router.post('/order', validateAuth, async (req, res) => {
 	}
 });
 
+// get all orders for a user
+router.get('/orders', validateAuth, async (req, res) => {
+	try {
+		const orders = await prisma.orders.findMany({
+			where: {
+				user: {
+					id: parseInt(req.body.userId),
+				},
+			},
+			include: {
+				recipe: true,
+				user: true,
+				ingredients: true,
+			},
+		});
+		if (orders === null) {
+			res.json({ message: 'No orders found' });
+		}
+		res.json({ message: 'Orders fetched', orders: orders });
+	} catch (err) {
+		console.log(err);
+	}
+});
+
+// cancel an order
+router.delete('/order/:id', validateAuth, async (req, res) => {
+	try {
+		const findOrder = await prisma.orders.findUnique({
+			where: {
+				id: parseInt(req.params.id),
+			},
+		});
+		if (findOrder === null) {
+			res.json({ message: 'Order not found' });
+		}
+		const order = await prisma.orders.delete({
+			where: {
+				id: parseInt(req.params.id),
+			},
+		});
+
+		res.json({ message: 'Order deleted', order: order });
+	} catch (err) {
+		console.log(err);
+	}
+});
+
 module.exports = router;
