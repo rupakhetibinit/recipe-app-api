a327e2e2857ed926c691bce2263bba0bfb36a371
Binit <cruiserponder@protonmail.com>
2021-12-25 23:58:25 +0545

    more cors bugs baby

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ corsOptions = {
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
 
 app.use(function (req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header(
 		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept'
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+		'Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS'
 	);
 	next();
 });
