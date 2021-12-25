49856047091e2f80727dc6b057969bd36042001b
Binit <cruiserponder@protonmail.com>
2021-12-26 00:01:19 +0545

    when will it end

--- index.js
+++ index.js
@@ -7,25 +7,9 @@ const orderRoute = require('./src/routes/orderRoute');
 const cors = require('cors');
 const app = express();
 const validateAuth = require('./src/middlewares/validateAuth');
-
-corsOptions = {
-	origin: 'https://svelte-recipe-admin.vercel.app/',
-	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-	successStatus: 200,
-};
-app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use(express.json());
-app.use(cors(corsOptions));
-
-app.use(function (req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-		'Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS'
-	);
-	next();
-});
+app.use(express.urlencoded({ extended: true }));
 dotenv.config();
 const port = process.env.PORT || 4000;
 
@@ -33,10 +17,10 @@ app.use('/check', async (req, res) => {
 	res.send('Successfully deployed').status(200);
 });
 
-app.use('/api/auth', authRoute);
-app.use('/api/v1', recipeRoute);
-app.use('/api/v1', likeRecipe);
-app.use('/api/v1', orderRoute);
+app.use('/api/auth', cors(), authRoute);
+app.use('/api/v1', cors(), recipeRoute);
+app.use('/api/v1', cors(), likeRecipe);
+app.use('/api/v1', cors(), orderRoute);
 
 app.listen(port, () => {
 	console.log(`Server is running on ${process.env.PORT}`);
