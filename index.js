6165f9b20e52eb265be96e44f9bc6b1a0426aabf
Binit <cruiserponder@protonmail.com>
2021-12-27 21:39:39 +0545

    fixed upload folder

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const fs = require('fs');
 
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
-		cb(null, './uploads/');
+		cb(null, path.join(__dirname, '/uploads'));
 	},
 	filename: function (req, file, cb) {
 		cb(null, file.originalname);
@@ -31,7 +31,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/uploads'));
-app.use('/uploads', express.static('/uploads'));
+app.use(express.static(__dirname));
 
 app.post(
 	'/image-upload-single',
